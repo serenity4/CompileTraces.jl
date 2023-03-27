14fcf5f5a42090bd8b1ef67da47794607e3d2219
Documenter.jl <documenter@juliadocs.github.io>
2023-03-27 21:17:20 +0000

    build based on af958ff

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CompileTraces","category":"page"},{"location":"#CompileTraces","page":"Home","title":"CompileTraces","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CompileTraces.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CompileTraces]","category":"page"}]
+}
